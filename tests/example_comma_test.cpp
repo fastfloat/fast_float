f70b64543602aa076b19a3deaacb484f9830be9c
Daniel Lemire <lemire@gmail.com>
2021-08-03 09:22:40 -0400

    Candidate release.

--- tests/example_comma_test.cpp
+++ tests/example_comma_test.cpp
@@ -3,9 +3,10 @@
 #include <iostream>
  
 int main() {
-    const std::string input =  "3.1416 xyz ";
+    const std::string input =  "3,1416 xyz ";
     double result;
-    auto answer = fast_float::from_chars(input.data(), input.data()+input.size(), result);
+    fast_float::parse_options options{fast_float::chars_format::general, ','};
+    auto answer = fast_float::from_chars_advanced(input.data(), input.data()+input.size(), result, options);
     if((answer.ec != std::errc()) || ((result != 3.1416))) { std::cerr << "parsing failure\n"; return EXIT_FAILURE; }
     std::cout << "parsed the number " << result << std::endl;
     return EXIT_SUCCESS;
