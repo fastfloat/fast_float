7f1c4a2f7f6998c83dca19d8f22df398cc6e68c6
Daniel Lemire <lemire@gmail.com>
2023-02-28 16:42:48 -0500

    This adds bloat analysis to the tests.

---
+++ tests/bloat_analysis/a9.cpp
@@ -0,0 +1,9 @@
+#include "fast_float/fast_float.h"
+
+
+double get9(const char* input) {
+    double result_value;
+    auto result = fast_float::from_chars(input, input + strlen(input), result_value);
+    if (result.ec != std::errc()) { return 9; }
+    return result_value;
+}
---
+++ tests/bloat_analysis/main.cpp
@@ -0,0 +1,19 @@
+
+
+double get1(const char* input);
+double get2(const char* input);
+double get3(const char* input);
+double get4(const char* input);
+double get5(const char* input);
+double get6(const char* input);
+double get7(const char* input);
+double get8(const char* input);
+double get9(const char* input);
+double get10(const char* input);
+
+int main(int arg, char** argv) {
+    double x = get1(argv[0]) + get2(argv[0]) + get3(argv[0]) + get4(argv[0]) + get5(argv[0])
+      + get6(argv[0]) + get7(argv[0]) + get8(argv[0]) + get9(argv[0]) + get10(argv[0]);
+
+    return int(x);
+}
