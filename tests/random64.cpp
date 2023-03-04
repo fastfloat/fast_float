58798ee81f478a0d56d008b79a54f80193781e6f
Lenard Szolnoki <leni536@gmail.com>
2023-03-04 17:23:20 +0000

    Fix failing test builds with -Werror=maybe-uninitialized on gcc
    Apparently the added constexpr makes gcc's control flow analysis to go deeper.
    
    If from_chars returns with error then the out parameter remains
    uninitialized. As it's undefined behavior to use its value, it's better
    to just skip the rest of the loop in this case.

--- tests/random64.cpp
+++ tests/random64.cpp
@@ -65,6 +65,7 @@ void random_values(size_t N) {
         if (errors > 10) {
           abort();
         }
+        continue;
       }
       if (std::isnan(v)) {
         if (!std::isnan(result_value)) {
