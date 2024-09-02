c9f8339668884044d915ce6fce058de3aefd2492
Evgeny Karpov <eukarpov@gmail.com>
2024-09-02 18:13:01 +0200

    Fix the formatting and remove the previous condition

--- include/fast_float/float_common.h
+++ include/fast_float/float_common.h
@@ -343,9 +343,8 @@ full_multiplication(uint64_t a, uint64_t b) {
   // But MinGW on ARM64 doesn't have native support for 64-bit multiplications
   answer.high = __umulh(a, b);
   answer.low = a * b;
-#elif defined(FASTFLOAT_32BIT) || (defined(_WIN64) && !defined(__clang__))
-#elif defined(FASTFLOAT_32BIT) || (defined(_WIN64) && !defined(__clang__) \
-  && !defined(_M_ARM64))
+#elif defined(FASTFLOAT_32BIT) ||                                              \
+    (defined(_WIN64) && !defined(__clang__) && !defined(_M_ARM64))
   answer.low = _umul128(a, b, &answer.high); // _umul128 not available on ARM64
 #elif defined(FASTFLOAT_64BIT) && defined(__SIZEOF_INT128__)
   __uint128_t r = ((__uint128_t)a) * b;
