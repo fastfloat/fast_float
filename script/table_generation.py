b9793e1cd4ffccb90e92178bc2ac9cd4564d6ca9
Anders Dalvander <anders.dalvander@gmail.com>
2024-11-23 11:24:39 +0100

    update scripts
    * fixed version updates in release script
    * formatted Python scripts with `black` - The uncompromising code formatter (https://black.readthedocs.io/)
    * f-string all the things

--- script/table_generation.py
+++ script/table_generation.py
@@ -1,14 +1,15 @@
 def format(number):
-    upper = number // (1<<64)
-    lower = number % (1<<64)
-    print(""+hex(upper)+","+hex(lower)+",")
+    upper = number // (1 << 64)
+    lower = number % (1 << 64)
+    print("" + hex(upper) + "," + hex(lower) + ",")
 
-for q in range(-342,0):
+
+for q in range(-342, 0):
     power5 = 5 ** -q
     z = 0
-    while( (1<<z) < power5) :
+    while (1 << z) < power5:
         z += 1
-    if(q >= -27):
+    if q >= -27:
         b = z + 127
         c = 2 ** b // power5 + 1
         format(c)
@@ -16,16 +17,16 @@ for q in range(-342,0):
         b = 2 * z + 2 * 64
         c = 2 ** b // power5 + 1
         # truncate
-        while(c >= (1<<128)):
-          c //= 2
+        while c >= (1 << 128):
+            c //= 2
         format(c)
 
-for q in range(0,308+1):
+for q in range(0, 308 + 1):
     power5 = 5 ** q
     # move the most significant bit in position
-    while(power5 < (1<<127)):
+    while power5 < (1 << 127):
         power5 *= 2
     # *truncate*
-    while(power5 >= (1<<128)):
+    while power5 >= (1 << 128):
         power5 //= 2
     format(power5)
