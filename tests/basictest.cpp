717112d2572546713422ecbfee88e89df6e8a6a2
Daniel Lemire <daniel@lemire.me>
2025-02-06 20:25:09 -0500

    lint

--- tests/basictest.cpp
+++ tests/basictest.cpp
@@ -509,9 +509,11 @@ bool check_file(std::string file_name) {
             std::cout << "as raw uint16_t, parsed = " << float16_parsed
                       << ", expected = " << float16 << std::endl;
             std::cout << "fesetround: " << round_name(d) << std::endl;
-            const bool is_ulfjack = file_name.find("ulfjack") != std::string::npos;
-            if(is_ulfjack) {
-              std::cout << "This is a known issue with ulfjack's test suite." << std::endl;
+            const bool is_ulfjack =
+                file_name.find("ulfjack") != std::string::npos;
+            if (is_ulfjack) {
+              std::cout << "This is a known issue with ulfjack's test suite."
+                        << std::endl;
             } else {
               fesetround(FE_TONEAREST);
               return false;
