a2437735b1396415372b571188609746c925a170
IRainman <a.rainman@gmail.com>
2025-03-24 12:38:49 +0300

    try to compilation fix on Linux

--- benchmarks/event_counter.h
+++ benchmarks/event_counter.h
@@ -129,7 +129,7 @@ struct event_collector {
   LinuxEvents<PERF_TYPE_HARDWARE> linux_events;
 
   event_collector()
-      : linux_events(std::vector<int>{
+      : linux_events(std::array<unsigned long long, event_counter_types_size>{
             PERF_COUNT_HW_CPU_CYCLES, PERF_COUNT_HW_INSTRUCTIONS,
             PERF_COUNT_HW_BRANCH_INSTRUCTIONS, // Retired branch instructions
             PERF_COUNT_HW_BRANCH_MISSES}) {}
