7eab29343626c689aaaea9573806255c53135361
IRainman <a.rainman@gmail.com>
2026-01-21 02:40:58 +0300

    fix for old C++ standard in library.

--- include/fast_float/float_common.h
+++ include/fast_float/float_common.h
@@ -439,7 +439,7 @@ fastfloat_strncasecmp(UC const *actual_mixedcase, UC const *expected_lowercase,
     else FASTFLOAT_IF_CONSTEXPR17(sizeof(UC) == 4) {
       mask = 0x0000002000000020;
     }
-    constexpr uint_fast8_t sz{8 / (sizeof(UC))};
+    constexpr uint_fast8_t sz = 8 / (sizeof(UC));
     for (uint_fast8_t i = 0; i < length; i += sz) {
       val1 = val2 = 0;
       sz = std::min(sz, static_cast<uint_fast8_t>(length - i));
