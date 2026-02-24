df5db2cfbf8765029e5728fa8ca68317723e20a1
Daniel Lemire <daniel@lemire.me>
2026-02-24 10:55:53 -0500

    adding C interface without dep to C++ standard lib

---
+++ src/fast_float_strtod.cpp
@@ -0,0 +1,24 @@
+#include "fast_float/fast_float_strtod.h"
+#include "fast_float/fast_float.h"
+#include <cerrno>
+#include <cstring>
+#include <system_error>
+
+extern "C" double fast_float_strtod(const char *nptr, char **endptr) {
+    double result = 0.0;
+
+    // Parse the string using fast_float's from_chars function
+    auto parse_result = fast_float::from_chars(nptr, nptr + strlen(nptr), result);
+
+    // Check if parsing encountered an error
+    if (parse_result.ec != std::errc{}) {
+        errno = EINVAL;
+    }
+
+    // Update endptr if provided
+    if (endptr != nullptr) {
+        *endptr = const_cast<char*>(parse_result.ptr);
+    }
+
+    return result;
+}
